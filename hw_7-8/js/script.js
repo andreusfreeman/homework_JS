abb8a785b30203d288fc2b3c5891bdc26e7a5e1f
andreusfreeman <andriy.natsyk@gmail.com>
2016-05-06 18:14:58 +0300

    done the first part homework 7-8

---
+++ hw_7-8/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.text-box-first').addClass('active');
+  $('.menu li:first').css('background-color','#ffffff')
+  $('.text-box-second, .text-box-third').hide();
+
+  $('.menu a').on('click', function(){
+    $('.menu li').css('background-color','#e9e9e9');
+    $(this).parents('.menu li').css('background-color','#ffffff');
+    var divName = $(this).attr('href');
+    $('.text-box-first, .text-box-second, .text-box-third').hide();
+    $('.'+divName).show();
+    return false;
+  })
+});
