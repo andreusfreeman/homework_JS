a14f32e7d8155bf0b7007a03730c20fd8b772d0c
andreusfreeman <andriy.natsyk@gmail.com>
2016-05-11 23:41:55 +0300

    done homework 7-8

---
+++ hw_7-8/js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('.text-box-first').addClass('active');
+  $('.menu li:first').css('background-color','#ffffff')
+  $('.text-box-second, .text-box-third').hide();
+
+  $('.menu a').on('click', function(){
+    $('.menu li').css('background-color','#e9e9e9');
+    $(this).parents('.menu li').css('background-color','#ffffff');
+    var divName = $(this).attr('href');
+    $('.text-box-first, .text-box-second, .text-box-third').hide();
+    $('.'+divName).show();
+    return false;
+  })
+
+  $(".form input").hover(function() {
+      $(this).next("em").animate({opacity: "show"}, "slow");
+  }, function() {
+      $(this).next("em").animate({opacity: "hide"}, "fast");
+  });
+
+  $('.button-info button').on('click', function(){
+    $("em").toggle();
+  });
+});
