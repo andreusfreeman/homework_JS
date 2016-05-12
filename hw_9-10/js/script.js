56eb445417044238138f98642ff8938ee4e56654
andreusfreeman <andriy.natsyk@gmail.com>
2016-05-12 23:01:10 +0300

    done all hw (without menu by JS)

--- hw_9-10/js/script.js
+++ hw_9-10/js/script.js
@@ -1,7 +1,23 @@
 $(function(){
-  $("#default-usage-select").selectbox().bind('change', function(){
-    $('<div>Value of #default-usage-select changed to: '+$(this).val()+'</div>').appendTo('#demo-default-usage .demoTarget').fadeOut(5000, function(){
-      $(this).remove();
-    });
-  });  
+
+  $( '.dropdown' ).hover(
+          function(){
+              $(this).children('.sub-menu').animate({
+                backgroundColor:"#03C",
+                color: "white",
+                height: "toggle"
+              }, 700);
+          },
+          function(){
+              $(this).children('.sub-menu').animate({
+                height: "toggle"
+              }, 350);
+          }
+      );
+
+$("#default-usage-select").selectbox().on('change', function(){
+  $('<div>Value of #default-usage-select changed to: '+$(this).val()+'</div>').appendTo('#demo-default-usage .demoTarget').fadeOut(5000, function(){
+    $(this).remove();
+  });
+});
 });
