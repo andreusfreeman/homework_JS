fc568576b8af6f6eae02ce3cafabaf15e4ff8482
andreusfreeman <andriy.natsyk@gmail.com>
2016-05-20 01:30:32 +0300

    done fours item

--- hw_13-14/js/script.js
+++ hw_13-14/js/script.js
@@ -1,3 +1,4 @@
+'use strict'
 var questions = {
   firstQuestion: 'Choice backend language',
   firstVariant: [
@@ -5,7 +6,7 @@ var questions = {
   ],
   secondQuestion: 'What programming languages are taught frontend-developers?',
   secondVariant: [
-    [ 'HTML/Java/CSS', false], ['Javascript/HTML/PHP', false], ['HTML/CSS/Javascript', false]
+    [ 'HTML/Java/CSS', false], ['Javascript/HTML/PHP', false], ['HTML/CSS/Javascript', true]
   ],
   thirdQuestion: 'What is the most popular framework/library on Javascript?',
   thirdVariant: [
@@ -13,3 +14,37 @@ var questions = {
   ]
 }
 localStorage.setItem('test', JSON.stringify(questions));
+var data = JSON.parse(localStorage.getItem('test'));
+
+$(function(){
+  var html = $('#easy-tmpl').html()
+  var content = tmpl(html, data);
+  $('.template-simple').append(content);
+});
+function checkResult(){
+  var rightAnswer = [];
+  for (var answ in data){
+    if (typeof data[answ] == 'object') {
+      for (var i = 0; i < data[answ].length; i++) {
+        rightAnswer.push(data[answ][i][1]);
+      }
+    }
+  }
+  var flag = false;
+  var a = document.getElementsByTagName('input');
+  for ( var i = 0; i < a.length; i++){
+    if (a[i].checked) {
+      if (rightAnswer[i]) {
+        flag = true;
+      } else {
+        break;
+      }
+    }
+  }
+  if (flag) {
+    console.log('OK');
+  }
+  else {
+    console.log('bad result!');
+  }
+}
