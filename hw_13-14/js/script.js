0b3ef229f1e12ff7b26c54b842934d9f3fa8449c
andreusfreeman <andriy.natsyk@gmail.com>
2016-05-22 23:11:37 +0300

    done homework 13-14

--- hw_13-14/js/script.js
+++ hw_13-14/js/script.js
@@ -21,12 +21,17 @@ $(function(){
   var content = tmpl(html, data);
   $('.template-simple').append(content);
 });
-function checkResult(){
+
+document.querySelector('.check-result').addEventListener('click', function(){
   var rightAnswer = [];
+  var checkQuantity = 0;
   for (var answ in data){
     if (typeof data[answ] == 'object') {
       for (var i = 0; i < data[answ].length; i++) {
         rightAnswer.push(data[answ][i][1]);
+        if (data[answ][i][1]){
+          checkQuantity += 1;
+        }
       }
     }
   }
@@ -36,15 +41,29 @@ function checkResult(){
     if (a[i].checked) {
       if (rightAnswer[i]) {
         flag = true;
+        checkQuantity -= 1;
       } else {
         break;
       }
     }
   }
-  if (flag) {
-    console.log('OK');
+  if (flag && checkQuantity === 0) {
+    var mod = document.querySelector('.modal-back');
+    document.querySelector('.result-info').innerHTML = 'WIN';
+    mod.style.display = 'block';
   }
   else {
-    console.log('bad result!');
+    var mod = document.querySelector('.modal-back');
+    document.querySelector('.result-info').innerHTML = 'You have done some mistakes! Try again.';
+    mod.style.display = 'block';
   }
-}
+})
+var elem = document.querySelector('.button-close');
+elem.addEventListener('click', function() {
+  document.querySelector('.modal-back').style.display = 'none';
+  document.querySelector('.result-info').innerHTML = '';
+  var checkBox = document.getElementsByTagName('input');
+  for ( var i = 0; i < checkBox.length; i++){
+    checkBox[i].checked = false;
+  }
+});
