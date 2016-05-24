952f772d8c3ed4eda36be55b6aa91ad115f5498f
andreusfreeman <andriy.natsyk@gmail.com>
2016-05-25 00:55:37 +0300

    done seven items homework

--- hw_15-16/js/script.js
+++ hw_15-16/js/script.js
@@ -1,1 +1,28 @@
 'use strict'
+document.body.addEventListener('keypress', function(event) {
+  if (document.querySelector('.search-query').value.length > 0 && event.keyCode === 13) {
+    $('#search-result > li').remove();
+    searchSomething(document.querySelector('.search-query').value);
+  }
+});
+document.querySelector('.search-button').addEventListener('click', function(event) {
+  if (document.querySelector('.search-query').value.length > 0) {
+    $('#search-result > li').remove();
+    searchSomething(document.querySelector('.search-query').value);
+  }
+});
+
+function searchSomething(query) {
+  $.ajax({
+           url: "https://pixabay.com/api/?key=2630733-330bf7ac78143b71181089b96&q=" + query + "&image_type=photo",
+           dataType : "jsonp",
+           success: function (data) {
+               var searchResult = document.getElementById('search-result');
+               for ( var i = 0; i < data.hits.length; i++) {
+                 var li = document.createElement('li');
+                 li.innerHTML = '<img src="' + data.hits[i].previewURL + '" width = "200" >';
+                 searchResult.appendChild(li);
+               }
+           }
+         });
+       }
