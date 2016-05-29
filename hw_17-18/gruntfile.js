1774ec8c572d2869953fa282aa395237b3c27506
andreusfreeman <andriy.natsyk@gmail.com>
2016-05-29 22:23:42 +0300

    done homework (without Gulp, it the next folder)

--- hw_17-18/gruntfile.js
+++ hw_17-18/gruntfile.js
@@ -10,13 +10,35 @@ module.exports = function(grunt) {
         // the files to concatenate
         src: ['js/*.js'],
         // the location of the resulting JS file
-        dest: 'js/script-concat.js'
+        dest: 'dest/script-concat.js'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['css/*.css'],
+        // the location of the resulting JS file
+        dest: 'dest/style-concat.css'
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'dest/script.min.js': ['dest/script-concat.js']
+        }
+      }
+    },
+    cssmin: {
+      my_target: {
+        files: {
+          'dest/style.min.css': ['dest/style-concat.css']
+        }
       }
     }
   });
 
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
